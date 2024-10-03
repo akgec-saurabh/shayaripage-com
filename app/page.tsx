6e69e8b7e729eb2be5dcde7439de4948cec3c09c
Saurabh Kumar <akgec.saurabh@gmail.com>
2024-10-04 00:21:55 +0530

    api/auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { auth } from "@/auth";
 import SignIn from "@/components/sign-in";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
+      {session?.user?.name}
+      {session?.user?.email}
       <SignIn />
     </div>
   );
