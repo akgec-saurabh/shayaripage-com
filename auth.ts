3cbc19ef139b5aeb41f86edbdfeccaa0435fb240
Saurabh Kumar <akgec.saurabh@gmail.com>
2024-10-04 00:36:37 +0530

    removed prisma

--- auth.ts
+++ auth.ts
@@ -1,10 +1,6 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { prisma } from "./prisma";
-import Resend from "next-auth/providers/resend";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [Google, Resend],
+  providers: [Google],
 });
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -12,15 +12,6 @@ export default function SignIn() {
       >
         <button type="submit">Signin with Google</button>
       </form>
-      <form
-        action={async (formData) => {
-          "use server";
-          await signIn("resend", formData);
-        }}
-      >
-        <input type="text" name="email" placeholder="Email" />
-        <button type="submit">Signin with Resend</button>
-      </form>
     </React.Fragment>
   );
 }
