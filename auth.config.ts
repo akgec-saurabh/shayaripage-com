c487d486468deac26931370104e37a4627bf508c
Saurabh Kumar <akgec.saurabh@gmail.com>
2024-10-04 06:11:43 +0530

    package.json for prisma generatesas

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,8 @@
 import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 // import Resend from "next-auth/providers/resend";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import prisma from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./prisma";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
@@ -12,7 +12,7 @@ export default {
     //   from: "admin@shayaripage.com",
     // }),
   ],
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   pages: {
     signIn: "/login",
